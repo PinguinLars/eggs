61af83ef5c404c39a9dec901842d60e006f8611b
Lars <pinguinlars1105@gmail.com>
2025-11-22 20:16:04 +0100

    Zellij and bar finished

--- eggs/assets/scripts/reload.sh
+++ eggs/assets/scripts/reload.sh
@@ -2,6 +2,6 @@
 ~/.config/yolk/ashy-dots/target/release/ashy-dots $HOME$(yolk eval 'get_wallpaper_path("")')
 hellwal -i ~/.config/share/wallpapers/mc_pride.png
 yolk sync || notify-send "dotfiles" "Error reloading!"
-swww img $HOME$(yolk eval 'get_wallpaper_path("")')
+swww img $HOME$(yolk eval 'get_wallpaper_path("")') -t none
 makoctl reload
 pkill -12 waybar
--- eggs/fish/conf.d/abbr.fish
+++ eggs/fish/conf.d/abbr.fish
@@ -1,3 +1,4 @@
 abbr --add za zellij a
 abbr --add su doas su
 abbr --add e exec 
+abbr --add cmd command
