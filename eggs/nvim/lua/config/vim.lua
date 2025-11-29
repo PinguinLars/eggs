0a284868ca10e22fb457ecdf3053275067dd0d2f
Lars <pinguinlars1105@gmail.com>
2025-11-29 12:56:33 +0100

    Small things

--- eggs/nvim/lua/config/vim.lua
+++ eggs/nvim/lua/config/vim.lua
@@ -6,7 +6,7 @@ vim.cmd("set number")
 vim.cmd("set relativenumber")
 vim.cmd("set clipboard+=unnamedplus")
 vim.cmd("set ignorecase")
-vim.cmd("cmap w!! w !SUDO_ASKPASS=/usr/libexec/openssh/ssh-askpass sudo -A tee %")
+vim.cmd("cmap w!! w !pkexec tee %")
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 vim.keymap.set('t', '<C-w>h', "<C-\\><C-n><C-w>h",{silent = true})
