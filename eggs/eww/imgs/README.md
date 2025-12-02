20758f52360de7fc631f791c6a36de23ef52bd2d
Lars <pinguinlars1105@gmail.com>
2025-12-02 18:22:36 +0100

    nvim-backup

---
+++ eggs/eww/imgs/README.md
@@ -0,0 +1,1 @@
+These are material design icons these are licensed under the APACHE 2.0 license
--- eggs/nvim/lua/plugins/discord.lua
+++ eggs/nvim/lua/plugins/discord.lua
@@ -2,7 +2,7 @@ return {
   'andweeb/presence.nvim',
   config = function ()
     require("presence").setup({
-      neovim_image_text = "Lightweight jebrains editor"
+      neovim_image_text = "Lightweight JetBrains editor"
     })
   end
 }
